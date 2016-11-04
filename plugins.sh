8a530b697f4472468f4926145f32092326e8dce2
Woody Hansen <woody@noreply.users.github.com>
2016-11-04 10:47:47 -0700

    Handlebars

--- plugins.sh
+++ plugins.sh
@@ -13,3 +13,5 @@ git submodule add git@github.com:scrooloose/syntastic.git syntastic.git
 git submodule add git@github.com:jiangmiao/auto-pairs.git auto-pairs.git
 git submodule add git@github.com:elzr/vim-json.git
 git submodule add git@github.com:editorconfig/editorconfig-vim
+# git submodule add git@github.com:wookiehangover/jshint.vim
+git submodule add git@github.com:mustache/vim-mustache-handlebars
